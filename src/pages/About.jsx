bb0237b079c66cf40e2783ddc9e39335743cbf4e
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2022-07-12 14:53:10 -0700

    Added timeline

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,8 @@ import StarParticle from "../components/StarParticle";
 import TechStack from "../components/TechStack";
 import ToolStack from "../components/ToolStack"
 import AboutCard from "../components/AboutCard"
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 
 function About() {
     return (
@@ -16,7 +18,38 @@ function About() {
                     </h1>
                     <AboutCard />
                 </Row>
-               
+                <h1 className="project-heading">
+                    My Humble <strong className="purple"> Beginning </strong>
+                </h1>
+                <VerticalTimeline>
+                    
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="September 2016 - September 2019"
+                        iconStyle={{ background: '#c770f0', color: '#fff' }}      
+                    >
+                        <h3 className="vertical-timeline-element-title">Computer Science</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Associate's Degree</h4>
+                        <p>Langara College</p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="September 2019 - April 2021"
+                        iconStyle={{ background: '#c770f0', color: '#fff' }}      
+                    >
+                        <h3 className="vertical-timeline-element-title">Freelancing</h3>
+                        <p>Freelancer.com</p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--education"
+                        date="April - June 2021"
+                        iconStyle={{ background: '#c770f0', color: '#fff' }}      
+                    >
+                        <h3 className="vertical-timeline-element-title">Full Stack Developer</h3>
+                        <p>Porton Health</p>
+                    </VerticalTimelineElement>
+                    
+                </VerticalTimeline>
                 <h1 className="project-heading">
                     Tech <strong className="purple">Skillset </strong>
                 </h1>
@@ -27,6 +60,7 @@ function About() {
                 </h1>
 
                 <ToolStack />
+                
             </Container>
         </Container>
     );
